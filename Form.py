e69145d5fae9174279955da54b1a8cefe80c3775
radiatus <radiatus@yandex.ru>
2017-01-04 16:49:57 +0300

    I did it

---
+++ Form.py
@@ -0,0 +1,105 @@
+# 235 строк | 211 строк на шарпах
+# Библиотеки
+import sys
+import Game
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QSpinBox
+from PyQt5.QtGui import QPainter, QBrush
+
+
+class FormGame(QMainWindow):
+
+    # Поля класса
+    __start_game = object
+    __rowsCount = object
+    __colsCount = object
+    __colorsCount = object
+    __scoreLabel = object
+    __game = object
+    # Набор цветов для кружков
+    __colors = [Qt.white, Qt.blue, Qt.red, Qt.yellow, Qt.green, Qt.magenta]
+
+    # Конструктор класса
+    def __init__(self):
+        super().__init__()
+        # Характеристика окна
+        self.title = "Game"
+        self.left = 100
+        self.top = 100
+        self.width = 500
+        self.height = 500
+        # Мне нужна эта переменная для нормальной работы графиик, почему-то как в шарпах не пашет, вылетает
+        self.helpIndex = 0
+        # Вызов отрисовки UI
+        self.init_ui()
+
+    # Метод отрисовки UI
+    def init_ui(self):
+        # Создаем окошко с параметрами из init
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # Создаем кнопку начала игры
+        self.__start_game = QPushButton('Start', self)
+        self.__start_game.move(400, 0)
+
+        # Создаем текст боксы для количества rows, cols, colors
+        self.__rowsCount = QSpinBox(self)
+        self.__rowsCount.move(400, 50)
+        self.__rowsCount.setValue(15)
+        self.__colsCount = QSpinBox(self)
+        self.__colsCount.move(400, 90)
+        self.__colsCount.setValue(15)
+        self.__colorsCount = QSpinBox(self)
+        self.__colorsCount.move(400, 130)
+
+        # Создаем label для очков
+        self.__scoreLabel = QLabel(self)
+        self.__scoreLabel.setText("Score")
+        self.__scoreLabel.move(400, 160)
+
+        # Привязка метода к нажатию кнопки
+        self.__start_game.clicked.connect(self.click_on_button)
+
+        self.show()
+
+    # Нажатие мышки на поле
+    def mousePressEvent(self, QMouseEvent):
+        if self.__game.click(QMouseEvent.pos().x(), QMouseEvent.pos().y()):
+            self.repaint()
+
+    # Реакция на клик по кнопке
+    def click_on_button(self):
+        self.helpIndex = 1
+        self.__game = Game.Game(self.__rowsCount.value(), self.__colsCount.value(), self.__colorsCount.value())
+        # Перерисуем графику
+        self.repaint()
+
+    # Отрисовка графики
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.paint(qp)
+        qp.end()
+
+    # Метод отрисовке всего поля с шарами
+    def paint(self, qp):
+        # Выходит из метода если игра не создана
+        if self.helpIndex == 0:
+            return
+
+        # Рисует поле шаров, если игра создана
+        brush = QBrush(Qt.SolidPattern)
+        self.__scoreLabel.setText(str(self.__game.score))
+        for y in range(self.__game.rowCounts):
+            for x in range(self.__game.columnCounts):
+                brush.setColor(self.__colors[self.__game.field[y][x]])
+                qp.setBrush(brush)
+                qp.drawEllipse(x * 25, y * 25, 25, 25)
+
+# Запуск как main
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = FormGame()
+    sys.exit(app.exec_())
---
+++ Game.py
@@ -0,0 +1,131 @@
+# Импортируем библиотечки
+import random
+import math
+
+
+# Класс Логики игры
+class Game:
+    # Поля Класса
+    __score = 0
+    __field = []
+    __index = 0
+    __row_index = 0
+    __col_index = 0
+    __rowCounts = 0
+    __columnCounts = 0
+    __colorsCounts = 0
+    __scoreColor = 1.2
+
+    # Ебанемка немного геттеров
+    @property
+    def score(self):
+        return self.__score
+
+    @property
+    def field(self):
+        return self.__field
+
+    @property
+    def rowCounts(self):
+        return self.__rowCounts
+
+    @property
+    def columnCounts(self):
+        return self.__columnCounts
+
+    @property
+    def colorsCounts(self):
+        return self.__colorsCounts
+
+    # Конструктор класса
+    def __init__(self, row_counts, column_counts, colors_counts):
+        self.__rowCounts = row_counts
+        self.__columnCounts = column_counts
+        self.__colorsCounts = colors_counts
+
+        self.__create_matrix()
+        self.__create_field()
+
+    # Метод для создания матрицы
+    def __create_matrix(self):
+        for row in range(self.__rowCounts):
+            self.field.append([])
+            for col in range(self.__columnCounts):
+                self.field[row].append(0)
+
+    # Метод для заполнение матрицы рандомными значениями
+    def __create_field(self):
+        for row in range(self.__rowCounts):
+            for col in range(self.__columnCounts):
+                self.field[row][col] = random.randint(1, self.__colorsCounts)
+
+    # Событие клика мышки
+    def click(self, col_index, row_index):
+        self.__row_index = int(row_index / 25)
+        self.__col_index = int(col_index / 25)
+
+        if self.__row_index < self.__rowCounts and self.__col_index < self.__columnCounts:
+            self.__neighbor(self.__row_index, self.__col_index, self.field[self.__row_index][self.__col_index])
+            self.__fall_rows()
+            self.__fall_columns()
+
+        if self.__index == 0:
+            return False
+        self.__index = 0
+        return True
+
+    # Определяет пустая ли колонка
+    def __is_column_empty(self, col_index):
+        for r in range(self.__rowCounts):
+            if self.field[r][col_index] != 0:
+                return False
+        return True
+
+    # Копирует колонку в колонку
+    def __copy_col_to_col(self, col_from, col_to):
+        for row in range(self.__rowCounts):
+            self.field[row][col_to] = self.field[row][col_from]
+
+    # Делает колонку пустой
+    def __set_col_zero(self, col):
+        for row in range(self.__rowCounts):
+            self.field[row][col] = 0
+
+    # Метод смещение при пустых колонках влево
+    def __fall_columns(self):
+        index1 = 0
+        for col in range(self.__columnCounts):
+            if self.__is_column_empty(col) == False:
+                self.__copy_col_to_col(col, index1)
+                index1 += 1
+
+        while index1 < self.__columnCounts:
+            self.__set_col_zero(index1)
+            index1 += 1
+
+    # Метод падения по стракам шариков
+    def __fall_rows(self):
+        for cols in range(self.__columnCounts):
+            index1 = self.__rowCounts - 1
+            for rows in range(self.__rowCounts-1, -1, -1):
+                if self.field[rows][cols] != 0:
+                    self.field[index1][cols] = self.field[rows][cols]
+                    index1 -= 1
+
+            for index1 in range(index1, -1, -1):
+                self.field[index1][cols] = 0
+
+    # Определяет соседние одинаковые круги и уничтожает их
+    def __neighbor(self, row_index, col_index, color_click):
+        f = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for i in range(0, 4):
+            if row_index + f[i][0] < self.__rowCounts and row_index + f[i][0] >= 0 and col_index + f[i][1] < self.__columnCounts and col_index + f[i][1] >= 0:
+                if (self.field[row_index + f[i][0]][col_index + f[i][1]] == color_click and self.field[row_index + f[i][0]][col_index + f[i][1]] != 0):
+                    self.field[row_index][col_index] = 0
+                    self.__index += 1
+                    self.__neighbor(row_index + f[i][0], col_index + f[i][1], color_click)
+
+        if self.__index == 0:
+            return
+        self.field[row_index][col_index] = 0
+        self.__score += int(math.pow(self.__scoreColor, self.__index))
